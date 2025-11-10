253043bdab645324efe66bff894d41cb9517fd1b
garrisonglass <garrison.glass@gmail.com>
2025-11-09 19:27:01 -0500

    It kind of works

--- Ch_5 Programs.cpp
+++ Ch_5 Programs.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int main()
 {
-	string Name, First,	Last;//Students name, if you ain't first you last
-
+	string name, first,	last;//Students name, if you ain't first you last
+	int count = 1;
 	ifstream InputFile;	// Define ifstream object
 
 
@@ -17,21 +17,24 @@ int main()
 		cout << "Error opening file!\n";
 	else
 	{
-		InputFile >> Name;// Read first name
-		First = Last = Name;
-
-		while (InputFile >> Name)// Read till end of file
+		InputFile >> name;// Read first name
+		first = name;
+		last = name;
+		
+		while (InputFile >> name)// Read till end of file
 		{
-			if (Name > Last)// To sort students by name
-				Last = Name;
-			if (Name < First)
-				First = Name;
-
-			InputFile >> Name;// Read next student
+			
+			if (name < first)// To sort students by name
+				first = name;
+			if (name > last)
+				last = name;
+			count++;
+			
 		}
 
-		cout << endl << First << " is at the front of the line.";// Display the student at the front of the line
-		cout << Last << " is at the end of the line.\n";// and which is at the back of the line.
+		cout << first << " is at the front of the line.\n";// Display the student at the front of the line
+		cout << last << " is at the end of the line.\n";// and which is at the back of the line.
+		cout << "There are " << count << " students." <<  endl;
 	}
 	InputFile.close();// Close file
 	return 0;
